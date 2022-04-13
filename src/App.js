f6343357f155f6a1f8f62ee5e7fa4b78fadbd0ae
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-12 23:07:55 -0400

    start Videoplayer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 
 function App() {
   return (
     <>
     <Header />
+    <main>
+      <VideoPlayer />
+    </main>
     </>
   );
 }
---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,12 @@
+import './VideoPlayer.scss';
+import '../../data/videos.json';
+function VideoPlayer() {
+    return (
+        <>
+        <video poster="" ></video>
+
+        </>
+    )
+}
+
+export default VideoPlayer;
