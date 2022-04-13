ac81d87f19afb7393eaed82e7c655c3f028de7d8
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-13 17:21:44 -0400

    fix folder structure

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,9 +1,21 @@
 import './VideoPlayer.scss';
-import '../../data/videos.json';
+import videoDetail from '../../data/video-details.json';
+
+
 function VideoPlayer() {
+
+
     return (
         <>
-        <video poster="" ></video>
+        <video poster="{videoDetail[0].image}">
+            <source src="{videoDetail[0].video}" type="video"/>
+        </video>
+        <div>
+            <p>{videoDetail[0].title}</p>
+            
+
+            <p>hello world</p>
+        </div>
 
         </>
     )
