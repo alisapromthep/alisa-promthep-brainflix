a84c0c930036e3f58687a9ab6b18b03260339fbd
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-19 22:51:36 -0400

    upload form and page styling for mobile completed

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './UploadForm.scss';
+import publish from '../../assets/icons/publish.svg';
 
 function UploadForm(props) {
     return (
         <form className='uploadform'>
             <label className='uploadform__label'>
                 title your video
-                <input className='uploadform__input' type='text' />
+                <input className='uploadform__input' type='text' placeholder='Add a title to your video' name='title' />
             </label>
             <label className='uploadform__label'>
                 add a video description
-                <textarea className='uploadform__input uploadform__input--large' type='text' />
+                <textarea className='uploadform__input uploadform__input--large' type='text' placeholder='Add a description to your video' name='description' />
             </label>
+            <div className='button__container'>
+                <img className='button__publish-icon' src={publish} alt='arrow up'/>
+                <button className='button'>publish</button>
+            </div>
+            <button className='button button--white'>cancel</button>
 
             
         </form>
--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -6,9 +6,9 @@ import UploadPreview from '../../assets/images/Upload-video-preview.jpg';
 function UploadPage(props) {
     return (
         <main className='upload'>
-            <h1> Upload Video </h1>
+            <h1 className='upload__header'> Upload Video </h1>
             <div className='upload__thumbnail-container'>
-                <p> video thumbnail </p>
+                <p className='upload__thumbnail-label'> video thumbnail </p>
                 <img className='upload__thumbnail-img' src={UploadPreview} alt='first person view of a biker looking down'/>
             </div>
             <UploadForm />
