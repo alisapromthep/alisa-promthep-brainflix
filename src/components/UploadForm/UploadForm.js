39e8e30ae79dc2503dc17fe6a055d80de176dd12
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-19 23:16:57 -0400

    tablet styling for upload page completed

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import './UploadForm.scss';
 import publish from '../../assets/icons/publish.svg';
+import UploadPreview from '../../assets/images/Upload-video-preview.jpg';
 
 function UploadForm(props) {
-    return (
-        <form className='uploadform'>
-            <label className='uploadform__label'>
-                title your video
-                <input className='uploadform__input' type='text' placeholder='Add a title to your video' name='title' />
-            </label>
-            <label className='uploadform__label'>
-                add a video description
-                <textarea className='uploadform__input uploadform__input--large' type='text' placeholder='Add a description to your video' name='description' />
-            </label>
-            <div className='button__container'>
-                <img className='button__publish-icon' src={publish} alt='arrow up'/>
-                <button className='button'>publish</button>
-            </div>
-            <button className='button button--white'>cancel</button>
 
-            
-        </form>
+    return (
+        <>
+            <div className='upload__thumbnail-container'>
+                <p className='upload__thumbnail-label'> video thumbnail </p>
+                <img className='upload__thumbnail-img' src={UploadPreview} alt='first person view of a biker looking down'/>
+            </div>
+            <form className='uploadform'>
+                <label className='uploadform__label'>
+                    title your video
+                    <input className='uploadform__input' type='text' placeholder='Add a title to your video' name='title' />
+                </label>
+                <label className='uploadform__label'>
+                    add a video description
+                    <textarea className='uploadform__input uploadform__input--large' type='text' placeholder='Add a description to your video' name='description' />
+                </label>
+                <div className='uploadform__button-container'>
+                    <div className='button__container'>
+                        <img className='button__publish-icon' src={publish} alt='arrow up'/>
+                        <button className='button'>publish</button>
+                    </div>
+                    <button className='button button--white'>cancel</button>
+                </div>
+            </form>
+        </>
     );
 }
 
--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './UploadPage.scss';
 import UploadForm from '../../components/UploadForm/UploadForm';
-import UploadPreview from '../../assets/images/Upload-video-preview.jpg';
 
 function UploadPage(props) {
     return (
         <main className='upload'>
             <h1 className='upload__header'> Upload Video </h1>
-            <div className='upload__thumbnail-container'>
-                <p className='upload__thumbnail-label'> video thumbnail </p>
-                <img className='upload__thumbnail-img' src={UploadPreview} alt='first person view of a biker looking down'/>
-            </div>
             <UploadForm />
         </main>
     );
