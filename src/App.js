8c9bed8ccaf688c18086a56133e4bdcafa28c241
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-21 22:05:28 -0400

    added link for brainflix logo to go to homepage and added link for upload button to go to upload page

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={HomePage}/> 
             <Route path="/upload" component={UploadPage} />
-            <Route path="/:video"/>
+            <Route path="/video/:videoId"/>
           </Switch>
         </BrowserRouter>
       </>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,12 @@ import brainflixLogo from '../../assets/logo/BrainFlix-logo.svg';
 import searchLogo from '../../assets/icons/search.svg';
 import mohanLogo from '../../assets/images/Mohan-muruge.jpg';
 import uploadLogo from '../../assets/icons/upload.svg';
-
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <header className='header'>
-            <img className='brainflix-logo' src={brainflixLogo} alt='BrainFlix Logo'/>
+            <Link to='/'><img className='brainflix-logo' src={brainflixLogo} alt='BrainFlix Logo'/></Link>
             <div className="header__container">
                 <div className='header__search-container'>
                     <form className = 'header__search-bar'>
@@ -18,8 +18,10 @@ function Header() {
                     <img className='header__search-img' src={mohanLogo} alt='mohan profile'/>
                 </div>
                 <div className='button__container'>
-                <img className='button__upload-icon' src={uploadLogo} alt='arrow pointing up for upload' />
-                <button className='button'>upload</button>
+                    <Link to='./upload'>
+                        <img className='button__upload-icon' src={uploadLogo} alt='arrow pointing up for upload' />
+                        <button className='button'>upload</button>
+                    </Link>
                 </div>
                 <img className='header__search-img header__search-img--tablet' src={mohanLogo} alt='mohan profile'/>
             </div>
