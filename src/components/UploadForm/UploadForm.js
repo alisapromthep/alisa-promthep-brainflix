42f7ccbc2e0e3eb5158405e5b8e58aaa71f6972a
Alisa Promthep <alisa.promthep@gmail.com>
2022-04-24 02:02:13 -0400

    adjust html, added container to achieve desktop styling for upload page, thumbnail photo and form still not aligned

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -15,14 +15,15 @@ function UploadForm({handleFormSubmit}) {
 
 
     return (
-        <div className='upload__container'>
-            <div className='upload__thumbnail-container'>
-                <p className='upload__thumbnail-label'> video thumbnail </p>
-                <img className='upload__thumbnail-img' src={UploadPreview} alt='first person view of a biker looking down'/>
+        <form 
+        onSubmit={handleFormSubmit}
+        className='uploadform'>
+        <div className='uploadform__container'>
+            <div className='uploadform__thumbnail-container'>
+                <p className='uploadform__thumbnail-label'> video thumbnail </p>
+                <img className='uploadform__thumbnail-img' src={UploadPreview} alt='first person view of a biker looking down'/>
             </div>
-            <form 
-            onSubmit={handleFormSubmit}
-            className='uploadform'>
+            <div className='uploadform__input-container'>
                 <label className='uploadform__label'>
                     title your video
                     <input className='uploadform__input' type='text' placeholder='Add a title to your video' name='title' />
@@ -31,15 +32,16 @@ function UploadForm({handleFormSubmit}) {
                     add a video description
                     <textarea className='uploadform__input uploadform__input--large' type='text' placeholder='Add a description to your video' name='description' />
                 </label>
-                <div className='uploadform__button-container'>
-                    <div className='button__container'>
-                        <img className='button__publish-icon' src={publish} alt='arrow up'/>
-                        <button type='submit' className='button'>publish</button>
-                    </div>
-                    <button type='button' className='button button--white'>cancel</button>
-                </div>
-            </form>
+            </div>
         </div>
+            <div className='uploadform__button-container'>
+                <div className='button__container'>
+                    <img className='button__publish-icon' src={publish} alt='arrow up'/>
+                    <button type='submit' className='button'>publish</button>
+                </div>
+                <button type='button' className='button button--white'>cancel</button>
+            </div>
+        </form>
     );
 }
 
